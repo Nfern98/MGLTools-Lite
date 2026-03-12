3271ca42293e8b806753485c3aa9bbaed4605802
Nicolas Fernandez Yañez <50000407+Nfern98@users.noreply.github.com>
2026-03-12 04:19:28 +0000

    Upgrade

--- mgltools_lite/__init__.py
+++
@@ -1,3 +0,0 @@
-from .prepare_ligand import prepare_ligand
-from .prepare_receptor import prepare_receptor
-from .batch_process import process_all_ligands, process_all_receptors
--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='mgltools-lite',
-    version='1.3',
-    packages=find_packages(),
-    install_requires=['rdkit','openbabel-wheel'],
-    description='Modern AutoDock4 ligand/receptor preparation tools',
-)
